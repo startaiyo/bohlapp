628c7fa34758522e6bcce197c71e1190936e2fe3
startaiyo <startaiyo0104@gmail.com>
2021-05-10 19:52:47 +0900

    inputタグの実装

--- bohlapp/src/components/submitform.js
+++ bohlapp/src/components/submitform.js
@@ -6,18 +6,17 @@ class Container extends Component {
   state ={
     selectedFile: null,
     keyname: null,
-    results: []
+    results: [],
+    f_data: []
   }
   fileSelectedHandler = event => {
-    console.log(event.target.files[0])
     this.setState({
       selectedFile: event.target.files[0]
     })
   } 
-  fileUploadHandler = e=>{
+  fileUploadHandler = e =>{
     const fd = new FormData();
     fd.append('image', this.state.selectedFile,this.state.selectedFile.name);
-    console.log(fd)
     axios.post('https://vyq3zznjx3.execute-api.ap-northeast-1.amazonaws.com/default/bohlfunc/',fd).then((response)=>{console.log(response); this.setState({keyname: response.data['key']})}).then(()=>{setTimeout(()=>{axios.get(`https://vyq3zznjx3.execute-api.ap-northeast-1.amazonaws.com/default/bohlfunc/${this.state.keyname}`).then( (res)=>{ console.log(res.data); this.setState({results: res.data})})},10000)}).catch((e)=>{console.log(e);})
     e.preventDefault();
   }
@@ -25,16 +24,32 @@ class Container extends Component {
     e.preventDefault();
     axios.get(`https://vyq3zznjx3.execute-api.ap-northeast-1.amazonaws.com/default/bohlfunc/${this.state.keyname}`).then( (res)=>{ console.log(res.data); this.setState({results: res.data})})
   }
+  dataRegister = e =>{
+    let num = Number(e.target.value)
+    console.log(this.state.results[num])
+    this.setState({
+      f_data: this.state.results[num]
+    })
+  }
+  dataSubmitter = e => {
+    e.preventDefault();
+    console.log(e.target.value);
+  }
   render(){
-    const rs = this.state.results.map((item)=><p>{item.food_name}</p>)
+    const rs = this.state.results.map((item,index)=><p><label key={index}><input type="radio" value={index} onChange={this.dataRegister} checked={this.state.f_data['food_name'] === item.food_name}/>{item.food_name}</label></p>)
     return (
-    <form>
-        <label htmlFor="img">画像</label>
-        <input id="img" type="file" onChange={this.fileSelectedHandler} encType="image/jpeg"/>
-        <button onClick={this.fileUploadHandler}>send</button>
-        <button onClick={this.dataGetter}>get</button>
+    <div> 
+      <form>
+          <label htmlFor="img">画像</label>
+          <input id="img" type="file" onChange={this.fileSelectedHandler} encType="image/jpeg"/>
+          <button onClick={this.fileUploadHandler}>send</button>
+          <button onClick={this.dataGetter}>get</button>
+      </form>
+      <form onSubmit={this.dataSubmitter}>
         <p>{rs}</p>
-    </form>
+        <button>今日の食事を更新</button>
+      </form>
+    </div>
    );
   }
 }
